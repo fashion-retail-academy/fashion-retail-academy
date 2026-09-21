3e3807481f74ee4c13511c4062ee295c9c993708
Rishi Tandon <tandonrishi2001@gmail.com>
2026-09-21 11:20:18 +0530

    Fix lesson progress import

--- app/learn/[slug]/[lesson]/page.tsx
+++ app/learn/[slug]/[lesson]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import LessonProgressLink from "./LessonProgressLink";
+import LessonProgressLink from "../LessonProgressLink";
 import { useParams } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
 import Link from "next/link";
